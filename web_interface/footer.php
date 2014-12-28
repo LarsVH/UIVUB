46a2a77e6097e8922e7873854b61eb1ec8e5618e
Filip Moons <fmoons@vub.ac.be>
2014-12-28 08:09:52 +0100

    Adding Wise Screen

---
+++ web_interface/footer.php
@@ -0,0 +1,144 @@
+
+        <script src="js/jquery-1.11.1.min.js"></script>
+        <script src="js/jquery-migrate-1.2.1.min.js"></script>
+        <script src="js/jquery-ui-1.10.3.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/modernizr.min.js"></script>
+        <script src="js/pace.min.js"></script>
+        <script src="js/retina.min.js"></script>
+        <script src="js/jquery.cookies.js"></script>
+        
+        <script src="js/bootstrap-wizard.min.js"></script>
+        <script src="js/jquery.validate.min.js"></script>
+        <script src="js/select2.min.js"></script>
+
+        <script src="js/custom.js"></script>
+        <script>
+            jQuery(document).ready(function() {
+                
+                // This will empty first option in select to enable placeholder
+                jQuery('select option:first-child').text('');
+                
+                // Select2
+                jQuery("select").select2({
+                    minimumResultsForSearch: -1
+                });
+                
+                // Basic Wizard
+                jQuery('#basicWizard').bootstrapWizard({
+                    onTabShow: function(tab, navigation, index) {
+                        tab.prevAll().addClass('done');
+                        tab.nextAll().removeClass('done');
+                        tab.removeClass('done');
+                        
+                        var $total = navigation.find('li').length;
+                        var $current = index + 1;
+                        
+                        if($current >= $total) {
+                            $('#basicWizard').find('.wizard .next').addClass('hide');
+                            $('#basicWizard').find('.wizard .finish').removeClass('hide');
+                        } else {
+                            $('#basicWizard').find('.wizard .next').removeClass('hide');
+                            $('#basicWizard').find('.wizard .finish').addClass('hide');
+                        }
+                    }
+                });
+                
+                // Progress Wizard
+                jQuery('#progressWizard').bootstrapWizard({
+                    onTabShow: function(tab, navigation, index) {
+                        tab.prevAll().addClass('done');
+                        tab.nextAll().removeClass('done');
+                        tab.removeClass('done');
+                        
+                        var $total = navigation.find('li').length;
+                        var $current = index + 1;
+                        
+                        if($current >= $total) {
+                            $('#progressWizard').find('.wizard .next').addClass('hide');
+                            $('#progressWizard').find('.wizard .finish').removeClass('hide');
+                        } else {
+                            $('#progressWizard').find('.wizard .next').removeClass('hide');
+                            $('#progressWizard').find('.wizard .finish').addClass('hide');
+                        }
+                        
+                        var $percent = ($current/$total) * 100;
+                        $('#progressWizard').find('.progress-bar').css('width', $percent+'%');
+                    }
+                });
+                
+                // Wizard With Disabled Tab Click
+                jQuery('#tabWizard').bootstrapWizard({
+                    onTabShow: function(tab, navigation, index) {
+                        tab.prevAll().addClass('done');
+                        tab.nextAll().removeClass('done');
+                        tab.removeClass('done');
+                        
+                        var $total = navigation.find('li').length;
+                        var $current = index + 1;
+                        
+                        if($current >= $total) {
+                            $('#tabWizard').find('.wizard .next').addClass('hide');
+                            $('#tabWizard').find('.wizard .finish').removeClass('hide');
+                        } else {
+                            $('#tabWizard').find('.wizard .next').removeClass('hide');
+                            $('#tabWizard').find('.wizard .finish').addClass('hide');
+                        }
+                    },
+                    onTabClick: function(tab, navigation, index) {
+                        return false;
+                    }
+                });
+                
+                // Wizard With Form Validation
+                jQuery('#valWizard').bootstrapWizard({
+                    onTabShow: function(tab, navigation, index) {
+                        tab.prevAll().addClass('done');
+                        tab.nextAll().removeClass('done');
+                        tab.removeClass('done');
+                        
+                        var $total = navigation.find('li').length;
+                        var $current = index + 1;
+                        
+                        if($current >= $total) {
+                            $('#valWizard').find('.wizard .next').addClass('hide');
+                            $('#valWizard').find('.wizard .finish').removeClass('hide');
+                        } else {
+                            $('#valWizard').find('.wizard .next').removeClass('hide');
+                            $('#valWizard').find('.wizard .finish').addClass('hide');
+                        }
+                    },
+                    onTabClick: function(tab, navigation, index) {
+                        return false;
+                    },
+                    onNext: function(tab, navigation, index) {
+                        var $valid = jQuery('#valWizard').valid();
+                        if (!$valid) {
+                            $validator.focusInvalid();
+                            return false;
+                        }
+                    }
+                });
+                
+                // Wizard With Form Validation
+                var $validator = jQuery("#valWizard").validate({
+                    highlight: function(element) {
+                        jQuery(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+                    },
+                    success: function(element) {
+                        jQuery(element).closest('.form-group').removeClass('has-error');
+                    }
+                });
+                
+                
+                // This will submit the basicWizard form
+                jQuery('.panel-wizard').submit(function() {    
+                    alert('This will submit the form wizard');
+                    return false // remove this to submit to specified action url
+                });
+
+            });
+        </script>
+
+    </body>
+</html>
---
+++ web_interface/index.php
@@ -0,0 +1,61 @@
+<?php include "header.php" ?>
+<div class="mainpanel">
+                    <div class="pageheader">
+                        <div class="media">
+                            <div class="pageicon pull-left">
+                                <i class="fa fa-home"></i>
+                            </div>
+                            <div class="media-body">
+                                <ul class="breadcrumb">
+                                    <li><a href="#"><i class="glyphicon glyphicon-home"></i></a></li>
+                                    <li>Dashboard</li>
+                                </ul>
+                                <h4>Dashboard</h4>
+                            </div>
+                        </div><!-- media -->
+                    </div><!-- pageheader -->
+                    
+                    <div class="contentpanel">
+                        
+                        <div class="row row-stat">
+                            <div class="col-md-12">
+                                <div class="panel panel-success-alt noborder">
+                                    <div class="panel-heading noborder">
+                                        <div class="panel-btns">
+                                            <a href="#" class="panel-close tooltips" data-toggle="tooltip" title="Close Panel"><i class="fa fa-times"></i></a>
+                                        </div><!-- panel-btns -->
+                                        <div class="panel-icon"><i class="fa fa-calendar-o"></i></div>
+                                        <div class="media-body">
+                                            <h5 class="md-title nomargin">Status</h5>
+                                            <h1 class="mt5">Available in F.10.718</h1>
+                                        </div><!-- media-body -->
+                                        <hr>
+                                        <div class="clearfix mt20">
+                                            <div class="pull-left">
+                                                <h5 class="md-title nomargin">Previous Status</h5>
+                                                <h4 class="nomargin">14u: Meeting with Filip</h4>
+                                            </div>
+                                            <div class="pull-right">
+                                                <h5 class="md-title nomargin">Next</h5>
+                                                <h4 class="nomargin">Coffee break with Lars</h4>
+                                            </div>
+                                        </div>
+                                        
+                                    </div><!-- panel-body -->
+                                </div><!-- panel -->
+                            </div><!-- col-md-4 -->
+                            
+                            
+                            
+                        </div><!-- row -->
+                        
+                        
+
+                       
+                            
+                            
+                           </div>
+            </div><!-- mainwrapper -->
+        </section>
+
+<?php include "footer.php"?>
